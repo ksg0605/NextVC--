20fbb49ff51d6b6b0e23079eb130827430354310
ksg0605 <ksg0605@gmail.com>
2023-08-31 16:07:01 +0900

    Initial Commit

---
+++ NextVC/FIrstViewController.swift
@@ -0,0 +1,66 @@
+//
+//  FIrstViewController.swift
+//  NextVC
+//
+//  Created by 김선규 on 2023/08/31.
+//
+
+import UIKit
+
+class FIrstViewController: UIViewController {
+
+    
+    var someString: String?
+    
+    let mainLabel: UILabel = {
+        let label = UILabel()
+
+        label.font = UIFont.systemFont(ofSize: 22)
+        return label
+    }()
+    
+    let backButton: UIButton = {
+        let button = UIButton(type: .custom)
+        button.setTitle("BACK", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = UIColor.blue
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+        button.addTarget(FIrstViewController.self, action: #selector(backButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setup()
+        makeAutoLayout()
+    }
+    
+    func setup() {
+        mainLabel.text = someString
+        
+        view.addSubview(mainLabel)
+        view.addSubview(backButton)
+        view.backgroundColor = .gray
+        
+    }
+    
+    func makeAutoLayout() {
+        mainLabel.translatesAutoresizingMaskIntoConstraints = false
+        backButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        mainLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        backButton.widthAnchor.constraint(equalToConstant: 70).isActive = true
+        backButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        backButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -40).isActive = true
+        backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    
+    @objc func backButtonTapped() {
+        
+    }
+
+}
